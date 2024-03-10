21a2105c869de9258102174f9f75c549d210bb6d
roma3 <roma38lorde@gmail.com>
2024-03-10 18:15:06 -0400

    created jaws in tc11 from tc1

---
+++ src/Packa/Jaws.java
@@ -0,0 +1,7 @@
+package Packa;
+
+public class Jaws {
+    public static void main(String[] args) {
+        System.out.println("kjdfhgjsdhfkjzsfjzdfshjdgfjzsgjtdjxdfjhsjdfgbdhsesmjdfvynkguirh6tkuey5ktwn3in4");
+    }
+}
