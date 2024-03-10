7408b9f6d507369e7e0abcd15416992c8a5164e3
roma3 <roma38lorde@gmail.com>
2024-03-10 18:29:25 -0400

    update

--- src/Setup/Fourth.java
+++ src/Setup/Fourth.java
@@ -2,12 +2,6 @@ package Setup;
 
 public class Fourth {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("kjfhskfjksf");
-=======
-        System.out.println("hdjhf");
-        System.out.println("hdh");
-        System.out.println("sdjhsdjfhs");
->>>>>>> TestCase002
+
     }
 }
