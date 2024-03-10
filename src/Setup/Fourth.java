d479162295bbea1f5a3ab6dec61305dcda22025a
roma3 <roma38lorde@gmail.com>
2024-03-10 18:10:08 -0400

    created 4 in tc2

---
+++ src/Setup/Fourth.java
@@ -0,0 +1,9 @@
+package Setup;
+
+public class Fourth {
+    public static void main(String[] args) {
+        System.out.println("hdjhf");
+        System.out.println("hdh");
+        System.out.println("sdjhsdjfhs");
+    }
+}
